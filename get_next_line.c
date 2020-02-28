39d7ad99f0fc33a3a00ff18472a0fbf572352fc4
Guilherme Marsi <gmarsi@c1r4p4.42sp.org.br>
2020-02-27 22:33:06 -0300

    GHub

--- get_next_line.c
+++ get_next_line.c
@@ -6,64 +6,79 @@
 /*   By: gmarsi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/12 21:20:16 by gmarsi            #+#    #+#             */
-/*   Updated: 2020/02/20 22:11:30 by gmarsi           ###   ########.fr       */
+/*   Updated: 2020/02/27 22:30:57 by gmarsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_get_line(char *str)
-{
-	int		i;
-	char	*linha;
-
-	i = 0;
-	if (!(linha = (char*)malloc(sizeof(char) * (ft_strlen(str) + 1))))
-		return (NULL);
-	while (str[i] != '\n' && str[i])
-	{
-		linha[i] = str[i];
-		i++;
-	}
-	linha[i] = '\0';
-	return (linha);
-}
-
-int		ft_check_line(char *str)
+int		ft_strpos(const char *str)
 {
 	int i;
 
 	i = 0;
+	if (!str)
+		return (0);
 	while (str[i])
 	{
 		if (str[i] == '\n')
-			return (1);
+			return (i);
 		i++;
 	}
-	return (0);
+	return (-1);
+}
+
+int		ret_frase(char **frase, char **line)
+{
+	int		posic;
+	char	*aux;
+
+	posic = ft_strpos(*frase);
+	if (posic >= 0)
+	{
+		*line = ft_substr(*frase, 0, posic);
+		aux = ft_substr(*frase, posic + 1, ft_strlen(*frase) - posic);
+		free(*frase);
+		*frase = aux;
+		aux = NULL;
+		return (1);
+	}
+	else
+		return (0);
+}
+
+int		ft_free_buffer(char *str, int ret)
+{
+	if (str)
+		free(str);
+	return (ret);
 }
 
 int		get_next_line(int fd, char **line)
 {
 	int			b_read;
-	char		buf[BUFFER_SIZE + 1];
+	char		*buf;
 	static char	*s[FD_SIZE];
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || !line)
+	if (fd < 0 || BUFFER_SIZE <= 0 || !line ||
+		(!(buf = (char*)malloc(sizeof(char) * (BUFFER_SIZE + 1)))))
 		return (-1);
+	if (s[fd] != NULL)
+	{
+		if (ret_frase(&s[fd], line))
+			return (ft_free_buffer(buf, 1));
+	}
+	else
+		s[fd] = ft_strdup("");
 	while ((b_read = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
 		buf[b_read] = '\0';
-		s[fd] = s[fd] == NULL ? ft_strdup(buf) : ft_strjoin(s[fd], buf);
-		if (ft_check_line(s[fd]))
-			break ;
+		if (!(s[fd] = ft_strjoin(s[fd], buf)))
+			return (-1);
+		if (ret_frase(&s[fd], line))
+			return (ft_free_buffer(buf, 1));
 	}
-	*line = ft_get_line(s[fd]);
-	if (ft_check_line(s[fd]) == 0 && b_read == 0)
-	{
-		free(s[fd]);
-		return (0);
-	}
-	s[fd] = ft_prepare_next(s[fd]);
-	return (1);
+	*line = b_read < 0 ? NULL : s[fd];
+	s[fd] = NULL;
+	return (ft_free_buffer(buf, b_read));
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,36 +6,11 @@
 /*   By: gmarsi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/12 21:21:05 by gmarsi            #+#    #+#             */
-/*   Updated: 2020/02/20 22:11:32 by gmarsi           ###   ########.fr       */
+/*   Updated: 2020/02/27 22:30:59 by gmarsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-char	*ft_prepare_next(char *str)
-{
-	int		i;
-	int		j;
-	char	*temp;
-
-	i = 0;
-	j = 0;
-	while (str[i] != '\n' && str[i] != '\0')
-		i++;
-	if (str[i] == '\0')
-		return (NULL);
-	if (!(temp = (char*)malloc(sizeof(char) * (ft_strlen(str) + 1 - i))))
-		return (NULL);
-	i++;
-	while (str[i + j])
-	{
-		temp[j] = str[i + j];
-		j++;
-	}
-	temp[j] = '\0';
-	free(str);
-	return (temp);
-}
 
 char	*ft_strdup(const char *s1)
 {
